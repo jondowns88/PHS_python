6d84530aebeaa9a183695c03fc3df38fa536614a
Downs <jdowns@kingcounty.gov>
2020-07-15 14:34:34 -0700

    Added multisubnetfailover to queries

--- run_strat.py
+++ run_strat.py
@@ -8,8 +8,7 @@ import sys
 #Import library for this project
 PYTHONPATH = sys.path.insert(0, os.getcwd())
 from model import get_clients as model
-
-conn = pyodbc.connect('DRIVER={SQL Server Native Client 11.0};SERVER=KCITEC2SQEPRP;DATABASE=php96;Trusted_Connection=yes')
+conn = pyodbc.connect('DRIVER={SQL Server Native Client 11.0};SERVER=KCITEC2SQEPRP;DATABASE=php96;Trusted_Connection=yes;MultiSubnetFailover=Yes;')
 
 dat = model.get_clients(conn, '2020-07-01', '2020-10-31')
 print(dat)
