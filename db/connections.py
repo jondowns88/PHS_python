32361fb2404b03796411275fc6b0aaf032a28dc6
Jon Downs <32069172+jondowns88@users.noreply.github.com>
2021-03-06 19:56:39 -0800

    Update connections.py

--- db/connections.py
+++ db/connections.py
@@ -7,7 +7,7 @@ import urllib
 def php96_engine(uid, pwd):
     #Make connection string
     string = urllib.parse.quote_plus("DRIVER={SQL Server Native client 11.0};"
-                                    r"SERVER=KCITEC2SQEPRP;DATABASE=php96;"
+                                    r"SERVER=YOUR_SERVER;DATABASE=YOURDB;"
                                     r"UID="+uid+";PWD="+pwd+";MultiSubnetFailover=Yes;")
 
     #Connect to php96
@@ -17,7 +17,7 @@ def php96_engine(uid, pwd):
 def phclaims_engine():
     #Make connection string
     string = urllib.parse.quote_plus("DRIVER={SQL Server Native client 11.0};"
-                                    r"SERVER=KCITSQLUTPDBH51;DATABASE=phclaims;"
+                                    r"SERVER=YOUR_SERVER;DATABASE=YOUR_DB;"
                                     r"Trusted_connection=yes;MultiSubnetFailover=Yes;")
     #Connect to php96
     engine = create_engine("mssql+pyodbc:///?odbc_connect=%s" % string)
@@ -26,8 +26,8 @@ def phclaims_engine():
 def hhsaw_engine():
     #Make connection string
     string = urllib.parse.quote_plus("DRIVER={ODBC Driver 17 for SQL Server};"
-                                    r"SERVER=kcitazrhpasqldev20.database.windows.net;"
-                                    r"DATABASE=hhs_analytics_workspace;"
+                                    r"SERVER=YOUR_SERVER;"
+                                    r"DATABASE=YOUR_DB;"
                                     r"Trusted_connection=yes;MultiSubnetFailover=Yes;")
     #Connect to php96
     engine = create_engine("mssql+pyodbc:///?odbc_connect=%s" % string)
